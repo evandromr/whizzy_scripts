b9f9373b6809aab7e660dc21cee142512f2bc09d
Abigail Stevens <alsteven@ualberta.ca>
2014-04-15 22:24:38 +0200

    Fiddled with, updated block comment

--- fits_info.py
+++ fits_info.py
@@ -5,41 +5,48 @@ from astropy.io import fits
 		fits_info.py
 	
 Prints HDU list information, extension 1 header keywords, and extension 1 column names 
-for a given FITS file. Using extension 1 here for RXTE data.
+for a given FITS file. Using extension 1 here for RXTE event-mode science data.
+
+Arguments:
+fits_file - str - The full path of the FITS file in question.
 
 Written in Python 2.7 by A.L. Stevens, A.L.Stevens@uva.nl, 2014
 
-Passed: fits_file - str - The full path of the FITS file.
+All scientific modules imported above, as well as python 2.7, can be downloaded in the 
+Anaconda package, https://store.continuum.io/cshop/anaconda/
+I don't think argparse came with Anaconda, but I don't remember installing anything 
+special to get it.
 
-Returns: nothing, but prints lots of stuff
-	
 """
 
 def main(fits_file):
 	pass
 	## Opens the fits file using the Astropy library 'fits.open'.
-	hdulist = fits.open(fits_file)
+	file_hdu = fits.open(fits_file)
 	## Print out the basic info on structure of FITS file.
-	print "\n", hdulist.info()
+	print "\n", file_hdu.info()
 
-	# print hdulist[0].header
-	# print hdulist[0].header.keys
-	# print hdulist[1].header
-	print "\n", hdulist[1].header.keys
-	print "\nColumns of data table:", hdulist[1].columns.names
+	# print file_hdu[0].header
+	# print file_hdu[0].header.keys
+	# print file_hdu[1].header
+	print "\n", file_hdu[1].header.keys
+	print "\nColumns of data table in ext 1:", file_hdu[1].columns.names
 	print ""
 
-	# print hdulist[2].header
+	# print file_hdu[2].header
 
-	# table = hdulist[1].data
-	# print table.field('RATE')
+# 	table = file_hdu[1].data
+# 	segment_rate = table[0:128].field(1).astype(int)
+# 	print segment_rate
+	
+	file_hdu.close()
 	
 	## End of function 'main'
 
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
-	parser.add_argument('fits_file', help="The full path of the FITS file.")
+	parser.add_argument('fits_file', help="The full path of the FITS file in question.")
 	args = parser.parse_args()
 
 	main(args.fits_file)
