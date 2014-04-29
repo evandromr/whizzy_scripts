4e68505eff98b85ad12f9f5463274d3dad2e8bdc
Abigail Stevens <A.L.Stevens@uva.nl>
2014-04-29 19:33:41 +0200

    Made it return the value so I can import and call it for use with other programs.

--- get_keyword.py
+++ get_keyword.py
@@ -11,6 +11,9 @@ variable=$(python "$script_dir"/get_keyword.py "$filter_file" 1 KEY)
 This finds the value of 'KEY' in the first ('1') extension of 'filter_file' and assigns 
 it to 'variable'. Keyword does not seem to be case-sensitive.
 
+Both prints and returns the value, for use in bash scripts like example above or 
+importing and calling in a python script. 
+
 Arguments:
 fits_file - str - The full path of the FITS file.
 ext - int - The FITS extension in which to search for the given keyword.
@@ -26,16 +29,18 @@ special to get it.
 
 """
 
+##################################
 def main(fits_file, ext, keyword):
 	pass
 	assert (ext >= 0 and ext <= 3)
-
 	hdulist = fits.open(fits_file)
-	print hdulist[ext].header[keyword]
+	key_value = hdulist[ext].header[keyword]
 	hdulist.close()
-	
+	print key_value
+	return key_value
 	## End of function 'main'
 
+##########################
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
@@ -44,6 +49,6 @@ if __name__ == "__main__":
 	parser.add_argument('keyword', help="The keyword for which you want the associated value.")
 	args = parser.parse_args()
 
-	main(args.fits_file, args.ext, args.keyword)
+	val = main(args.fits_file, args.ext, args.keyword)
 
 ## End of program 'get_keyword.py'
