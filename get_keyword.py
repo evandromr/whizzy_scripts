d9a00836903ce3241dcbea3a5f30f83eed7fb007
Abigail Stevens <A.L.Stevens@uva.nl>
2014-05-06 12:50:21 +0200

    Created toggle switch for printing value.

--- get_keyword.py
+++ get_keyword.py
@@ -4,20 +4,23 @@ from astropy.io import fits
 """
 		get_keyword.py
 	
-Gets the a keyword value from a FITS header.
+Gets the value of a keyword from a FITS header.
 
 Use this in a bash script like so:
-variable=$(python "$script_dir"/get_keyword.py "$filter_file" 1 KEY)
+variable=$(python "$script_dir"/get_keyword.py "$filter_file" 1 KEY True)
 This finds the value of 'KEY' in the first ('1') extension of 'filter_file' and assigns 
 it to 'variable'. Keyword does not seem to be case-sensitive.
 
 Both prints and returns the value, for use in bash scripts like example above or 
-importing and calling in a python script. 
+importing and calling in a python script. For calling in a python script, set print_it 
+to False.
 
 Arguments:
 fits_file - str - The full path of the FITS file.
 ext - int - The FITS extension in which to search for the given keyword.
 keyword - str - The keyword for which you want the associated value.
+print_it - int - 1 if you want to print the value (like with a bash script), 0 if 
+	you don't want to print the value (like with a python script).
 
 
 Written in Python 2.7 by A.L. Stevens, A.L.Stevens@uva.nl, 2014
@@ -36,7 +39,6 @@ def main(fits_file, ext, keyword):
 	hdulist = fits.open(fits_file)
 	key_value = hdulist[ext].header[keyword]
 	hdulist.close()
-	print key_value
 	return key_value
 	## End of function 'main'
 
@@ -47,8 +49,13 @@ if __name__ == "__main__":
 	parser.add_argument('fits_file', help="The full path of the FITS file.")
 	parser.add_argument('ext', type=int, help="The FITS extension in which to search for the given keyword.")
 	parser.add_argument('keyword', help="The keyword for which you want the associated value.")
+	parser.add_argument('print_it', type=int, help="1 if you want to print the value, 0 if you don't.")
 	args = parser.parse_args()
-
+	assert (args.print_it == 1 or args.print_it == 0)
+	
 	val = main(args.fits_file, args.ext, args.keyword)
+	
+	if args.print_it == 1:
+		print val
 
 ## End of program 'get_keyword.py'
