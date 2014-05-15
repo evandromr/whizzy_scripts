868adf176012745795a9900113c8efb38c3e3745
Abigail Stevens <A.L.Stevens@uva.nl>
2014-05-15 15:12:08 +0200

    Updated get_keyword call, updated block comment.

--- total_obs_time.py
+++ total_obs_time.py
@@ -4,27 +4,33 @@ import argparse
 """
 		total_obs_time
 
-Passed: file_list - str - List of FITS observation files. One file per line.
+Arguments:
+file_list - str - Name of (ASCII/txt) file with list of FITS observation files. 
+	One file per line.
 
-Returns: total_time - float - The total observational time of all observations in the 
-			list.
+Written in Python 2.7 by A.L. Stevens, A.L.Stevens@uva.nl, 2014
+
+get_keyword is available in my Git repository 'whizzy_scripts'
 			
 """
+
 ##########################	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('file_list', \
-		help="List of fits files with observations")
+		help="Name of file with list of fits files of the observations.")
 	args = parser.parse_args()
 
 	input_files = [line.strip() for line in open(args.file_list)]
 	
 	total_time = 0
 	for file in input_files:
-		start_time = float(get_keyword.main(file, 0, 'TSTART', 0))
-		stop_time = float(get_keyword.main(file, 0, 'TSTOP', 0))
+		start_time = float(get_keyword.main(file, 0, 'TSTART'))
+		stop_time = float(get_keyword.main(file, 0, 'TSTOP'))
 		time = stop_time - start_time
 		total_time += time
-	print total_time
-# 	return total_time
-	## End of function 'total_obs_time'
+	
+	print total_time # in seconds
+
+
+## End of function 'total_obs_time'
