fa7d304e784e6e22e7160139044ba2652049b685
Abigail Stevens <A.L.Stevens@uva.nl>
2014-05-15 14:50:50 +0200

    Got rid of redundant `main` method, put it all in __main__

--- fits_info.py
+++ fits_info.py
@@ -19,10 +19,14 @@ special to get it.
 	
 """
 
-def main(fits_file):
-	pass
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('fits_file', help="The full path of the FITS file in question.")
+	args = parser.parse_args()
+	
 	## Opens the fits file using the Astropy library 'fits.open'.
-	file_hdu = fits.open(fits_file)
+	file_hdu = fits.open(args.fits_file)
 	## Print out the basic info on structure of FITS file.
 	print "\n", file_hdu.info()
 
@@ -32,23 +36,22 @@ def main(fits_file):
 	print "\n", file_hdu[1].header.keys
 	print "\nColumns of data table in ext 1:", file_hdu[1].columns.names
 	print ""
-
+	print file_hdu[1].data[0]
+	
+# 	cols=file_hdu[1].columns.names
+# 	for col in cols:
+# 		if "pcu2" in col.lower():
+# 			print col
+# 			
 # 	table = file_hdu[1].data
 # 	segment_rate = table[0:128].field(1).astype(int)
 # 	print segment_rate
+
+# 	for i in range(0,10):
+# 		print file_hdu[1].data[i].field(0)
 	
-	print file_hdu[1].data
-	
+	num_points=len(file_hdu[1].data.field(1))
+# 	print file_hdu[1].data[num_points-1].field(0)
+
 	file_hdu.close()
-	
-	## End of function 'main'
-
-if __name__ == "__main__":
-	
-	parser = argparse.ArgumentParser()
-	parser.add_argument('fits_file', help="The full path of the FITS file in question.")
-	args = parser.parse_args()
-
-	main(args.fits_file)
-
 ## End of program 'fits_info.py'
